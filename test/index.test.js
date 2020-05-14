c41e68dc5dfe403fb23768a3ea20020d1de5e09a
Jonathan Hefner <jonathan@hefner.pro>
2020-05-14 00:01:39 -0500

    Leverage nock to make assertions

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,6 @@ const path = require("path")
 const { Probot } = require("probot")
 const appFn = require("..")
 const payload = require("./fixtures/issues.opened")
-const issueCreatedBody = { body: "Thanks for opening this issue!" }
 
 describe("verba-sequentur", () => {
   let probot
@@ -22,13 +21,13 @@ describe("verba-sequentur", () => {
 
   test("creates a comment when an issue is opened", async () => {
     nock("https://api.github.com")
-      .post("/repos/jonathanhefner/testing-things/issues/1/comments", (body) => {
-        expect(body).toMatchObject(issueCreatedBody)
-        return true
-      })
+      .post("/repos/jonathanhefner/testing-things/issues/1/comments", ({ body }) =>
+        body == "Thanks for opening this issue!"
+      )
       .reply(200)
 
     await probot.receive({ name: "issues", payload })
+    expect(nock.isDone()).toBe(true)
   })
 
   afterEach(() => {
