5fbd7e1e496626e11eea50855dea12cc57d2ab7e
Jonathan Hefner <jonathan@hefner.pro>
2020-05-13 23:04:40 -0500

    Remove boilerplate comments

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,6 @@
 const nock = require("nock")
-// Requiring our app implementation
 const myProbotApp = require("..")
 const { Probot } = require("probot")
-// Requiring our fixtures
 const payload = require("./fixtures/issues.opened")
 const issueCreatedBody = { body: "Thanks for opening this issue!" }
 const fs = require("fs")
@@ -23,17 +21,14 @@ describe("My Probot app", () => {
   beforeEach(() => {
     nock.disableNetConnect()
     probot = new Probot({ id: 123, cert: mockCert })
-    // Load our app into probot
     probot.load(myProbotApp)
   })
 
   test("creates a comment when an issue is opened", async () => {
-    // Test that we correctly return a test token
     nock("https://api.github.com")
       .post("/app/installations/2/access_tokens")
       .reply(200, { token: "test" })
 
-    // Test that a comment is posted
     nock("https://api.github.com")
       .post("/repos/jonathanhefner/testing-things/issues/1/comments", (body) => {
         expect(body).toMatchObject(issueCreatedBody)
@@ -41,7 +36,6 @@ describe("My Probot app", () => {
       })
       .reply(200)
 
-    // Receive a webhook event
     await probot.receive({ name: "issues", payload })
   })
 
@@ -50,9 +44,3 @@ describe("My Probot app", () => {
     nock.enableNetConnect()
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
