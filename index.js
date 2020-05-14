94d8aa3cba514177498fa8770604ef59f6e515d7
Jonathan Hefner <jonathan@hefner.pro>
2020-05-14 18:03:23 -0500

    Support configurable response per label

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
  */
 module.exports = app => {
   app.on(["issues.labeled", "pull_request.labeled"], async context => {
+    const config = await context.config("verba-sequentur.yml", {})
     const label = context.payload.label.name
-    const comment = context.issue({ body: label })
-    return context.github.issues.createComment(comment)
+    const cannedResponse = config[label]
+    if (cannedResponse) {
+      const comment = context.issue({ body: cannedResponse.comment })
+      return context.github.issues.createComment(comment)
+    }
   })
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,36 +1,58 @@
 const nock = require("nock")
 const fs = require("fs").promises
 const path = require("path")
+const yaml = require("js-yaml")
 const { Probot } = require("probot")
 const appFn = require("..")
 const webhookEvents = require("./fixtures/webhook-events.json")
+const repoUrlPath = "/repos/jonathanhefner/testing-things"
 
 describe("verba-sequentur", () => {
   let probot
   let mockCert
+  let config
+  let configYaml
 
   beforeAll(async () => {
     mockCert = await fs.readFile(path.join(__dirname, "fixtures/mock-cert.pem"))
+    configYaml = await fs.readFile(path.join(__dirname, "fixtures/config.yml"))
+    config = yaml.safeLoad(configYaml)
   })
 
   beforeEach(() => {
     nock.disableNetConnect()
+
+    nock("https://api.github.com")
+      .get(`${repoUrlPath}/contents/.github/verba-sequentur.yml`)
+      .reply(200, { content: Buffer.from(configYaml).toString("base64") })
+
     probot = new Probot({ id: 123, cert: mockCert })
     probot.load(appFn)
   })
 
   test.each([
     "issue labeled foo",
-    "pull request labeled foo"
+    "pull request labeled foo",
+    "issue labeled bar",
+    "pull request labeled bar"
   ])("handles %s", async (scenario) => {
     const event = webhookEvents[scenario]
     const label = event.payload.label.name
-    expect(label).toBeTruthy() // sanity check
+    const comment = config[label].comment
 
     nock("https://api.github.com")
-      .post("/repos/jonathanhefner/testing-things/issues/1/comments", ({ body }) => body == label)
+      .post(`${repoUrlPath}/issues/1/comments`, ({ body }) => body == comment)
       .reply(200)
 
+    await probot.receive(event)
+    expect(nock.isDone()).toBe(true)
+  })
+
+  test.each([
+    "issue labeled zzz",
+    "pull request labeled zzz"
+  ])("ignores %s", async (scenario) => {
+    const event = webhookEvents[scenario]
     await probot.receive(event)
     expect(nock.isDone()).toBe(true)
   })
