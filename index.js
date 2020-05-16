1fc736d971d6ad794a0adf64420d65278e4eade5
Jonathan Hefner <jonathan@hefner.pro>
2020-05-16 17:48:48 -0500

    Add logging

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
  */
 module.exports = app => {
   app.on(["issues.labeled", "pull_request.labeled"], async context => {
+    const label = context.payload.label.name
+    const log = context.log.child(context.issue({ label }))
+
     const issue = context.payload.issue || context.payload.pull_request
     if (issue.state == "closed" || issue.locked) {
+      log("Ignoring closed / locked issue")
       return
     }
-
-    const label = context.payload.label.name
 
     const listEvents = context.github.issues.listEvents.endpoint.merge(context.issue())
     for await (const { data: page } of context.github.paginate.iterator(listEvents)) {
       if (page.some(item => item.event == "unlabeled" && item.label.name == label)) {
+        log("Ignoring re-application of label")
         return
       }
     }
@@ -20,12 +23,23 @@ module.exports = app => {
     const config = await context.config("verba-sequentur.yml", {})
     const cannedResponse = config[label]
 
-    if (cannedResponse && cannedResponse.comment) {
+    if (!cannedResponse) {
+      log("No response configured for label")
+      return
+    }
+
+    if (Object.keys(cannedResponse).some(key => key != "comment" && key != "close")) {
+      log.warn("Unrecognized configuration label")
+    }
+
+    if (cannedResponse.comment) {
+      log("Creating comment")
       const comment = context.issue({ body: cannedResponse.comment })
       await context.github.issues.createComment(comment)
     }
 
-    if (cannedResponse && cannedResponse.close) {
+    if (cannedResponse.close) {
+      log("Closing issue")
       const close = context.issue({ state: "closed" })
       return context.github.issues.update(close)
     }
